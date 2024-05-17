9c24a0a4a4efdae45e0aaf331b6c03a33fa14518
Eric Acquah <ericacquah1720@gmail.com>
2024-05-17 18:01:51 +0000

    Adding boilerplate code

---
+++ charter4less/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import React from 'react';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from './components/ui/Header/Header';
+import NavbarComponent from './components/ui/Navbar/NavbarComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+    </div>
+  );
+}
+
+export default App;
