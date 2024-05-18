5880ad46cf05b6561c2b715170cb73b93a4bf716
Eric Acquah <ericacquah1720@gmail.com>
2024-05-18 23:26:32 +0000

    Making progress in landding page

--- charter4less/src/pages/Landingpage/HeroSection.js
+++ charter4less/src/pages/Landingpage/HeroSection.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
+import './HeroSection.css';
 
 
 export default function HeroSection() {
   return (
     <section className="hero-section">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h1>Charter4Less</h1>
-            <p>Book your next charter bus with Charter4Less. We offer the best prices and the best service.</p>
-            <Button variant="primary">Book Now</Button>
+      <Container className="hero-container">
+        <Row className="content-row">
+          <Col className="text-col" md={6}>
+            <h1>Borrow, Lend, Share: Your Local Rental Hub!</h1>
+            <p>Need something? Find it here. Got something? Rent it out. Join your neighbors in making sharing simple and fun!</p>
+            <Button variant="primary">Rent Now</Button>
           </Col>
-          <Col md={6}>
+          <Col className="img-col" md={6}>
             <img src="https://via.placeholder.com/500" alt="Charter Bus" />
           </Col>
+        </Row>
+        <Row>
+          <Container className="searchbox-container">
+          <Col xs={6}>
+            <Form>
+              <Form.Control className="serch-box" type="text" placeholder="Search" />
+            </Form>
+          </Col>
+          </Container>
         </Row>
       </Container>
     </section>
--- charter4less/src/pages/Landingpage/LandingPage.js
+++ charter4less/src/pages/Landingpage/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from './HeroSection';
 import Header from '../../components/ui/Header/Header';
 import FooterComponent from '../../components/ui/Footer/FooterComponent';
+import './Landingpage.css'
 
 export default function LandingPage() {
   return (
