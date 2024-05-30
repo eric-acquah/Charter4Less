97f6bd065614ec4790554ec4412746d818891d53
evalase219 <chiomaevangeline85@gmail.com>
2024-05-30 14:47:11 +0200

    Implementation of about us cards

---
+++ charter4less/src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './AboutUs.css';
+import { Card, Col, Row, Button, Container } from 'react-bootstrap';
+
+export default function AboutUs() {
+    return (
+        <div>
+            <Container>
+                 {/* Row for Card 1 and Card 2 */}
+                 <Row>
+                    <Col md={6}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title 1</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title 2</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                {/* Row for Card 3 */}
+                <Row className="justify-content-md-center">
+                    <Col md={4}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title 3</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
