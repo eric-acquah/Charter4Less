0aa4915fe3dfba47a4515bb42a2d556c59554a51
Eric Acquah <ericacquah1720@gmail.com>
2024-05-30 17:39:38 +0000

    Made slight chages to how it works page

--- charter4less/src/pages/HowItWorks/HowItWorks.js
+++ charter4less/src/pages/HowItWorks/HowItWorks.js
@@ -10,13 +10,13 @@ export default function HowItWorks() {
     const [activeTab, setActiveTab] = useState('owners');
 
     return (
-        <div>
+        <section className='how-it-works-section'>
             <Nav variant="tabs" activeKey={activeTab} onSelect={(selectedKey) => setActiveTab(selectedKey)}>
                 <Nav.Item>
-                    <Nav.Link eventKey="owners">For Owners</Nav.Link>
+                    <Nav.Link className='nav-link-btn' eventKey="owners">For Owners</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="renters">For Renters</Nav.Link>
+                    <Nav.Link className='nav-link-btn' eventKey="renters">For Renters</Nav.Link>
                 </Nav.Item>
             </Nav>
             <div className="howitworks">
@@ -53,7 +53,7 @@ export default function HowItWorks() {
                     {activeTab === 'renters' && (
                         <Row>
                             <Col>
-                                <HowItWorksHeader text="Step A" />
+                                <HowItWorksHeader text="Step 1" />
                                 <p>
                                     Aliquam erat volutpat. Integer sit amet volutpat tortor. Nullam
                                     at elit ac justo cursus consequat. Vestibulum ante ipsum primis
@@ -61,7 +61,7 @@ export default function HowItWorks() {
                                 </p>
                             </Col>
                             <Col>
-                                <HowItWorksHeader text="Step B" />
+                                <HowItWorksHeader text="Step 2" />
                                 <p>
                                     Curabitur eget leo at velit imperdiet varius. In eu ipsum
                                     vitae velit congue iaculis vitae at risus. Lorem ipsum dolor
@@ -69,7 +69,7 @@ export default function HowItWorks() {
                                 </p>
                             </Col>
                             <Col>
-                                <HowItWorksHeader text="Step C" />
+                                <HowItWorksHeader text="Step 3" />
                                 <p>
                                     Donec tincidunt nunc a nisi tincidunt, a lobortis libero
                                     vestibulum. Proin vel augue nec risus convallis vestibulum.
@@ -78,11 +78,11 @@ export default function HowItWorks() {
                             </Col>
                         </Row>
                     )}
-                </Container>
-                <Button className="button" style={{ position: 'fixed', bottom: '10px' }}>
-                    Button
+                    <Button className="how-it-works-btn button" >
+                    {activeTab === 'owners' ? 'List Item' : 'Rent Item'}
                 </Button>
+                </Container>
             </div>
-        </div>
+        </section>
     );
 }
---
+++ charter4less/src/pages/HowItWorks/HowItWorkspage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/ui/Header/Header';
+import Footer from '../../components/ui/Footer/FooterComponent';
+import HowItWorks from './HowItWorks';
+
+
+export default function HowItWorkspage() {
+    return (
+        <>
+            <Header />
+            <HowItWorks />
+            <Footer />
+        </>
+    )
+}
