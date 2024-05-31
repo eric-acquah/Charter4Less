6ac2c496bd43a89866ead1572627474b8b864b0e
evalase219 <chiomaevangeline85@gmail.com>
2024-05-31 08:54:31 +0200

    Implementation of about us card title

--- charter4less/src/pages/AboutUs/AboutUs.js
+++ charter4less/src/pages/AboutUs/AboutUs.js
@@ -12,7 +12,7 @@ export default function AboutUs() {
                         <Card className="card-1" style={{ width: '25rem', height: '15rem' }}>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Card Title 1</Card.Title>
+                                <Card.Title className='card-title-1'>WHAT IS Charter4less?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
@@ -25,7 +25,7 @@ export default function AboutUs() {
                         <Card className="card-2" style={{ width: '25rem', height: '15rem' }}>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Card Title 2</Card.Title>
+                                <Card.Title className='card-title-2'>HOW IT WORKS?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
@@ -41,7 +41,7 @@ export default function AboutUs() {
                         <Card className="card-3" style={{ width: '25rem', height: '15rem' }}>
                             <Card.Img variant="top" src="holder.js/100px180" />
                             <Card.Body>
-                                <Card.Title>Card Title 3</Card.Title>
+                                <Card.Title className='card-title-3'>MADE FOR WHO?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
