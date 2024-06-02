bd629cca6c9a748e1b59597b0de97f3500b69520
evalase219 <chiomaevangeline85@gmail.com>
2024-06-02 22:16:33 +0200

    Implementation of about us page

--- charter4less/src/pages/AboutUs/AboutUs.js
+++ charter4less/src/pages/AboutUs/AboutUs.js
@@ -4,33 +4,29 @@ import { Card, Col, Row, Button, Container } from 'react-bootstrap';
 
 export default function AboutUs() {
     return (
-        <div>
+        <section>
             <Container className="about-us-container">
                 {/* Row for Card 1 and Card 2 */}
                 <Row>
                     <Col md={6} className="card-col-1">
-                        <Card className="card-1" style={{ width: '25rem', height: '15rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card className="card-custom">
                             <Card.Body>
                                 <Card.Title className='card-title-1'>WHAT IS Charter4less?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col md={6} className="card-col-2">
-                        <Card className="card-2" style={{ width: '25rem', height: '15rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card className="card-custom">
                             <Card.Body>
                                 <Card.Title className='card-title-2'>HOW IT WORKS?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
                             </Card.Body>
                         </Card>
                     </Col>
@@ -38,20 +34,19 @@ export default function AboutUs() {
                 {/* Row for Card 3 */}
                 <Row className="justify-content-md-center">
                     <Col md={4} className="card-col-3">
-                        <Card className="card-3" style={{ width: '25rem', height: '15rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card className="card-custom">
                             <Card.Body>
                                 <Card.Title className='card-title-3'>MADE FOR WHO?</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Button className='card-title-3-button'>Button</Button>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </section>
     );
 }
