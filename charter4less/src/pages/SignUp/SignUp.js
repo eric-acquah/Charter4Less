530fc133d440a87606c1a33ab0ea7afb18c85694
evalase219 <chiomaevangeline85@gmail.com>
2024-06-06 16:51:42 +0200

    Implementation of sign up page

---
+++ charter4less/src/pages/SignUp/SignUp.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import './SignUp.css';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+export default function SignUp() {
+
+  const schema = Yup.object().shape({
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required('Last name is required'),
+    username: Yup.string().required('Username is required'),
+    city: Yup.string().required('City is required'),
+    state: Yup.string().required('State is required'),
+    zip: Yup.string().required('Zip code is required'),
+    file: Yup.mixed().required('File is required'),
+    terms: Yup.bool().required('Terms must be accepted').oneOf([true], 'Terms must be accepted'),
+  });
+
+  return (
+    <Formik
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        username: '',
+        city: '',
+        state: '',
+        zip: '',
+        file: null,
+        terms: false,
+      }}
+    >
+      {({ handleSubmit, handleChange, setFieldValue, values, touched, errors }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationFormik101" className="position-relative">
+              <Form.Label>First name</Form.Label>
+              <Form.Control
+                type="text"
+                name="firstName"
+                value={values.firstName}
+                onChange={handleChange}
+                isValid={touched.firstName && !errors.firstName}
+                isInvalid={touched.firstName && !!errors.firstName}
+              />
+              <Form.Control.Feedback type="invalid" tooltip>{errors.firstName}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormik102" className="position-relative">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+                isValid={touched.lastName && !errors.lastName}
+                isInvalid={touched.lastName && !!errors.lastName}
+              />
+              <Form.Control.Feedback type="invalid" tooltip>{errors.lastName}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormikUsername2">
+              <Form.Label>Username</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  isInvalid={touched.username && !!errors.username}
+                />
+                <Form.Control.Feedback type="invalid" tooltip>{errors.username}</Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="6" controlId="validationFormik103" className="position-relative">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+                isInvalid={touched.city && !!errors.city}
+              />
+              <Form.Control.Feedback type="invalid" tooltip>{errors.city}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik104" className="position-relative">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="State"
+                name="state"
+                value={values.state}
+                onChange={handleChange}
+                isInvalid={touched.state && !!errors.state}
+              />
+              <Form.Control.Feedback type="invalid" tooltip>{errors.state}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik105" className="position-relative">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Zip"
+                name="zip"
+                value={values.zip}
+                onChange={handleChange}
+                isInvalid={touched.zip && !!errors.zip}
+              />
+              <Form.Control.Feedback type="invalid" tooltip>{errors.zip}</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Form.Group className="position-relative mb-3">
+            <Form.Label>File</Form.Label>
+            <Form.Control
+              type="file"
+              name="file"
+              onChange={(event) => {
+                setFieldValue('file', event.currentTarget.files[0]);
+              }}
+              isInvalid={touched.file && !!errors.file}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>{errors.file}</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="position-relative mb-3">
+            <Form.Check
+              required
+              name="terms"
+              label="Agree to terms and conditions"
+              onChange={handleChange}
+              isInvalid={touched.terms && !!errors.terms}
+              feedback={errors.terms}
+              feedbackType="invalid"
+              id="validationFormik106"
+              feedbackTooltip
+            />
+          </Form.Group>
+          <Button type="submit">Submit form</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
